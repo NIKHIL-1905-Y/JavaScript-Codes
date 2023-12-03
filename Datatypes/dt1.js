955a83394b3b1f3df21c51dc10c53aab3010bdd6
unknown <yadavnishu1905@gmail.com>
2023-12-03 12:36:00 +0530

    updated dt1

--- Datatypes/dt1.js
+++ Datatypes/dt1.js
@@ -61,3 +61,13 @@ myFunction()
 
 console.log(typeof(myFunction))
 
+//Type of ALL Data Types
+console.log(typeof 10); // number
+console.log(typeof 'Hello'); // string
+console.log(typeof true); // boolean
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof {}); // object
+console.log(typeof []); // object
+console.log(typeof function() {}); // function
+console.log(typeof new Date()); // object
