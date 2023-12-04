abd5dd033475aaab981ac85e4f008c675736f02e
unknown <yadavnishu1905@gmail.com>
2023-12-04 08:31:00 +0530

    Heap and Stack Memory

---
+++ Stack & Heap/sthp.js
@@ -0,0 +1,33 @@
+//Stack Memory ::(Primitive data types)
+
+console.log("\nStack ::\n")
+
+let myName = "Nikhil"
+
+let myName2 = myName; //Copying
+
+console.log(myName2)
+
+myName2 = "Nishu";
+
+console.log(myName2)
+console.log(myName)
+
+
+//Heap Memory ::(Non Primitive data Types)
+
+console.log("\nHeap ::\n")
+
+let userdemo = {
+    email:"user@gmail.com",
+    upi:"user@ybl"
+}
+
+console.log(userdemo.email)
+
+let userOne = userdemo;  //reference to original value
+
+userOne.email = "nikhil@gmail.com"
+
+console.log(userOne.email)
+console.log(userdemo.email)
